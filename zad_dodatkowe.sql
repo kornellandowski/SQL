e812293c7420eeebe68ef48f3aadcc79fe9823cb
Kornel Landowski <kornellandowski@gmail.com>
2024-07-21 19:06:59 +0200

    Update zad_dodatkowe.sql

--- zad_dodatkowe.sql
+++ zad_dodatkowe.sql
@@ -1,4 +1,11 @@
-With combineData as (
+/*
+Task Description:
+
+By combining data from four tables, identify the campaign with the highest Return on Marketing Investment (ROMI) among all campaigns with total expenditures exceeding 500,000. 
+Within this campaign, identify the ad set group (adset_name) with the highest ROMI.
+*/
+
+With combine_data as (
 select 
 ad_date,
 campaign_name,
@@ -8,14 +15,14 @@ from fabd
 INNER JOIN facebook_campaign ON facebook_campaign.campaign_id = fabd.campaign_id
 INNER JOIN facebook_adset ON facebook_adset.adset_id  = fabd.adset_id 
 ),
-UnionData as (
+union_data as (
 select
 ad_date,
 'Facebook ads' as media_source,
 campaign_name,
 adset_name,
 spend, impressions, reach, clicks, leads, value
-from combineData
+from combine_data 
 union
 select
 ad_date,
@@ -25,12 +32,12 @@ adset_name,
 spend, impressions, reach, clicks, leads, value
 from google_ads_basic_daily gabd 
 ),
-agregetedData as(
+agregeted_data as(
 select
 adset_name,
 sum(spend) as total_koszt,
 (sum(value) - sum(spend)) * 1.0 / sum(spend) * 100 as ROMI
-from UnionData
+from union_data
 group by
 adset_name
 )
@@ -38,7 +45,7 @@ select
 adset_name,
 max(ROMI) as max_ROMI
 from 
-agregetedData
+agregeted_data
 where 
 total_koszt > 500000
 group by
